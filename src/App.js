fbb939c0013e67729063bd13cd59ccedab9287d5
Aravind V V <arvndvv@gmail.com>
2021-10-23 13:13:19 +0530

    dark mode

--- src/App.js
+++ src/App.js
@@ -7,9 +7,11 @@ export default class App extends Component {
     super();
   this.state = {
     monsters: [],
-    searchField: ''
+    searchField: '',
+    darkMode:false
   }
   }
+
   componentDidMount() {
     fetch('https://jsonplaceholder.typicode.com/users')
     .then(response => response.json())
@@ -17,13 +19,33 @@ export default class App extends Component {
     
   }
 
+  handleSearch = (e) =>{
+    this.setState({ searchField: e.target.value })
+  }
+  toggleDarkMode = () => {
+    this.setState({darkMode: !this.state.darkMode})
+  }
+
   render() {
-    const { monsters, searchField } = this.state;
+    const { monsters, searchField, darkMode } = this.state;
     const filteredMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLowerCase()));
     return (
      
-      <div className="App">
-        <Search placeholder="search" handleChange={e=>this.setState({ searchField: e.target.value })} />
+      <div className={`App ${darkMode ? 'dark-mode' : ''}`}>
+        <div className="header">
+        <div>
+    <input type="checkbox" className="checkbox" id="checkbox" onChange={this.toggleDarkMode}/>
+  <label htmlFor="checkbox" className="label">
+    <i className='fas fa-sun'></i>
+    <i className="fas fa-moon"></i>
+    <div className='ball'>
+</div>
+  </label>
+  </div>
+        <h1 className="heading">Monsters Rolodex</h1>
+        <span className="dark-mode-toggle" onClick={this.toggleDarkMode}></span>
+        </div>
+        <Search placeholder="search" handleChange={this.handleSearch} />
         <CardList monsters={filteredMonsters}/>
     </div>
     )
